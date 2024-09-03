f3c3157633fb408175fe5fc14ebb418981dcd03d
kestsaurav21 <kestsaurav21@outlook.com>
2024-09-04 01:00:33 +0530

    single selection

--- accordion/src/components/Accordion.jsx
+++ accordion/src/components/Accordion.jsx
@@ -1,30 +1,51 @@
 import React, { useState } from 'react'
 import data from '../utils/mockData'
 
+
+
+
 const Accordion = () => {
 
     const [select, setSelect] = useState(null);
 
     const handleSingle = (id) => {
-        setSelect(id);
+        setSelect(id === select ? null : id)
         
     }
+
   return (
-    <div className='w-[80%] min-h-[900px] bg-gray-400 m-20 p-2 rounded-xl'>
-        
-            <h3 className='text-black font-extrabold text-4xl text-center p-4'>Accordion</h3>
+    <div className='bg-gray-400 w-[80%] h-[900px] m-20 rounded-xl p-4 flex-col '>
+        <h2 className='text-black font-extrabold text-4xl text-center m-4'>Accordion</h2>
+        <div className='flex justify-center'>
+            <button className='bg-black text-white p-4 rounded-2xl cursor-pointer'>
+                Enable Multi Selection
+            </button>
+        </div>
+
+        <div className='accordion'>
+
             {
-                data && data.length > 0 
-                ? (data.map((item) => 
-                <div onClick={()=>handleSingle(item.id)}
-                className='flex justify-between p-4'>
-                    <h3>{item.question}</h3>
-                    <span className='cursor-pointer'>+</span>
-                </div>
-                ) )
-                : <div>No Data Found</div>
+                data && data.length > 0 ?  
+                    data.map((item) => 
+                        <div className=''>
+                            <div onClick={() => handleSingle(item.id)}
+                            className='m-10 flex justify-between cursor-pointer text-xl font-bold '>
+                            <h3>{item.question}</h3>
+                            <span>+</span>
+                            </div>
+                            {
+                                select === item.id  ?
+                                <h3 className='m-10 text-center'>{item.answer}</h3>
+                                : null
+                            }
+                        </div>
+                    )
+                
+                : <div>No data found</div>
             }
-        
+
+        </div>
+
     </div>
   )
 }
